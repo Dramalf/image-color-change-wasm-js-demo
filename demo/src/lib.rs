b3dae8fb4f14302d110a68d4b81707995752b66d
Dramalf <dramalfcsc@gmail.com>
2024-05-14 21:56:27 +0800

    feat:add webgl

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -10,94 +10,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use hsl::HSL;
-use num_complex::Complex64;
-use rand::Rng;
-use rayon::prelude::*;
-use wasm_bindgen::{prelude::*, Clamped};
-
 #[cfg(feature = "parallel")]
 pub use wasm_bindgen_rayon::init_thread_pool;
 
-type RGBA = [u8; 4];
-
-struct Generator {
-    width: u32,
-    height: u32,
-    palette: Box<[RGBA]>,
-}
-
-impl Generator {
-    fn new(width: u32, height: u32, max_iterations: u32) -> Self {
-        let mut rng = rand::thread_rng();
-
-        Self {
-            width,
-            height,
-            palette: (0..max_iterations)
-                .map(move |_| {
-                    let (r, g, b) = HSL {
-                        h: rng.gen_range(0.0..360.0),
-                        s: 0.5,
-                        l: 0.6,
-                    }
-                    .to_rgb();
-                    [r, g, b, 255]
-                })
-                .collect(),
-        }
-    }
-
-    #[allow(clippy::many_single_char_names)]
-    fn get_color(&self, x: u32, y: u32) -> &RGBA {
-        let c = Complex64::new(
-            (f64::from(x) - f64::from(self.width) / 2.0) * 4.0 / f64::from(self.width),
-            (f64::from(y) - f64::from(self.height) / 2.0) * 4.0 / f64::from(self.height),
-        );
-        let mut z = Complex64::new(0.0, 0.0);
-        let mut i = 0;
-        while z.norm_sqr() < 4.0 {
-            if i == self.palette.len() {
-                return &self.palette[0];
-            }
-            z = z.powi(2) + c;
-            i += 1;
-        }
-        &self.palette[i]
-    }
-
-    fn iter_row_bytes(&self, y: u32) -> impl '_ + Iterator<Item = u8> {
-        (0..self.width)
-            .flat_map(move |x| self.get_color(x, y))
-            .copied()
-    }
-
-    fn iter_bytes(&self) -> impl '_ + ParallelIterator<Item = u8> {
-        (0..self.height)
-            // Note: when built without atomics, into_par_iter() will
-            // automatically fall back to single-threaded mode.
-            .into_par_iter()
-            .flat_map_iter(move |y| self.iter_row_bytes(y))
-    }
-}
-
-#[wasm_bindgen]
-pub fn generate(width: u32, height: u32, max_iterations: u32) -> Clamped<Vec<u8>> {
-    Clamped(
-        Generator::new(width, height, max_iterations)
-            .iter_bytes()
-            .collect(),
-    )
-}
 use std::collections::HashMap;
 use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use rayon::prelude::*;
-
 #[macro_use]
 extern crate lazy_static;
-
-
 
 #[wasm_bindgen]
 extern "C" {
@@ -122,23 +43,16 @@ macro_rules! log {
 }
 
 lazy_static! {
-    pub static ref GlobalBufferStorage: Mutex<BufferStorage> = {
+    pub static ref GLOBAL_BUFFER_STORAGE: Mutex<BufferStorage> = {
         let buffer_storage = BufferStorage::new();
         Mutex::new(buffer_storage)
     };
 }
-
-// #[wasm_bindgen]
-// pub fn set_wasm_panic_hook() {
-//     // can be continued
-//     set_panic_hook();
-// }
-
 #[wasm_bindgen]
 pub fn new_buffer(key: String, len: usize) -> *const u8 {
     log!("new_buffer, key: {:?}, len: {:?}", key, len);
-    let mut global_buffer_storage = GlobalBufferStorage.lock().unwrap();
-    let mut buffer = vec![15; len];
+    let mut global_buffer_storage = GLOBAL_BUFFER_STORAGE.lock().unwrap();
+    let buffer = vec![0; len];
     let ptr = buffer.as_ptr();
     global_buffer_storage.buffer_map.insert(key, buffer);
     ptr
@@ -146,7 +60,7 @@ pub fn new_buffer(key: String, len: usize) -> *const u8 {
 
 #[wasm_bindgen]
 pub fn get_buffer(key: String) -> *const u8 {
-    let global_buffer_storage = GlobalBufferStorage.lock().unwrap();
+    let global_buffer_storage = GLOBAL_BUFFER_STORAGE.lock().unwrap();
     if let Some(buffer) = global_buffer_storage.buffer_map.get(&key) {
         return buffer.as_ptr();
     } else {
@@ -156,7 +70,7 @@ pub fn get_buffer(key: String) -> *const u8 {
 
 #[wasm_bindgen]
 pub fn print_buffer(key: String) {
-    let global_buffer_storage = GlobalBufferStorage.lock().unwrap();
+    let global_buffer_storage = GLOBAL_BUFFER_STORAGE.lock().unwrap();
     if let Some(buffer) = global_buffer_storage.buffer_map.get(&key) {
         log!("[render-wasm]print buffer: {:?}", buffer);
     }
@@ -164,7 +78,7 @@ pub fn print_buffer(key: String) {
 
 #[wasm_bindgen]
 pub fn remove_buffer(key: String) {
-    let mut global_buffer_storage = GlobalBufferStorage.lock().unwrap();
+    let mut global_buffer_storage = GLOBAL_BUFFER_STORAGE.lock().unwrap();
     if let Some(_) = global_buffer_storage.buffer_map.remove(&key) {
         log!("remove buffer success");
     } else {
@@ -193,7 +107,7 @@ fn hue2rgb(p: f32, q: f32, t: f32) -> f32 {
 }
 
 fn hsl_to_rgb(h: f32, s: f32, l: f32) -> (u8, u8, u8) {
-    let (mut r, mut g, mut b): (f32, f32, f32);
+    let (r, g, b): (f32, f32, f32);
 
     if s == 0.0 {
         r = l;
@@ -210,34 +124,36 @@ fn hsl_to_rgb(h: f32, s: f32, l: f32) -> (u8, u8, u8) {
     ((r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8)
 }
 
-
 #[wasm_bindgen]
-pub fn harmonize_rayon(key:String, ch: f32, cs: f32) {
-    let mut global_buffer_storage = GlobalBufferStorage.lock().unwrap();
+pub fn harmonize_rayon(key: String, ch: f32, cs: f32) {
+    let mut global_buffer_storage = GLOBAL_BUFFER_STORAGE.lock().unwrap();
     if let Some(buffer) = global_buffer_storage.buffer_map.get_mut(&key) {
-    buffer.par_chunks_mut(4).for_each(|chunk|{
-        let (r, g, b) = (chunk[0] as f32 / 255.0, chunk[1] as f32 / 255.0, chunk[2] as f32 / 255.0);
-        let max = r.max(g).max(b);
-        let min = r.min(g).min(b);
-        let l = (max + min) / 2.0;
-        let s: f32 = if cs>0.1{
-            cs
-        }
-        else if max == min {
-            0.0
-        } else if l > 0.5 {
-            (max - min) / (2.0 - max - min)
-        } else {
-            (max - min) / (max + min)
-        };
-        // // 将HSL值转换回RGB
-        let (new_r, new_g, new_b)= hsl_to_rgb(ch, s, l);
+        buffer.par_chunks_mut(4).for_each(|chunk| {
+            let (r, g, b) = (
+                chunk[0] as f32 / 255.0,
+                chunk[1] as f32 / 255.0,
+                chunk[2] as f32 / 255.0,
+            );
+            let max = r.max(g).max(b);
+            let min = r.min(g).min(b);
+            let l = (max + min) / 2.0;
+            let s: f32 = if cs > 0.1 {
+                cs
+            } else if max == min {
+                0.0
+            } else if l > 0.5 {
+                (max - min) / (2.0 - max - min)
+            } else {
+                (max - min) / (max + min)
+            };
+            // // 将HSL值转换回RGB
+            let (new_r, new_g, new_b) = hsl_to_rgb(ch, s, l);
 
-        // 更新图像数据
-        chunk[0] = new_r;
-        chunk[1] = new_g;
-        chunk[2] = new_b;
-    })
+            // 更新图像数据
+            chunk[0] = new_r;
+            chunk[1] = new_g;
+            chunk[2] = new_b;
+        })
     } else {
         return ();
     }
